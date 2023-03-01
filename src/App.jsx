92330615b02af3c77aa8e6793e9c33498c588d9d
Sparrow48 <abunasib48@gmail.com>
2023-03-01 22:13:29 +0600

    Add Navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './router/Navigation';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-
-    </div>
+    <Router>
+      <div className='flex flex-col space-y-16'>
+        <div className='sticky top-0'>
+          <Header />
+        </div>
+        <Routes>
+          <Route path={'/*'} element={<Navigation />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import ThemeSwitch from '../components/ThemeSwitch'
+import { Navbar, Button } from "flowbite-react";
+
+const Header = () => {
+    const textStyle = `font-normal text-gray-700 tracking-wider transition-color hover:text-yellow-500 text-sm`
+    return (
+
+        <Navbar
+            fluid={true}
+            rounded={true}
+
+            className="sticky top-0 z-20 max-w-4xl py-2 mx-auto bg-white md:py-6 md:mb-6 dark:bg-black"
+        >
+            <Navbar.Brand>
+                <NavLink to="/">
+                    <a className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                        Md. Abu Nasib
+                    </a>
+                </NavLink>
+
+            </Navbar.Brand>
+            <div className="flex md:order-2">
+                <ThemeSwitch />
+                <Navbar.Toggle className="-mt-1" />
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link>
+                    <NavLink to='/' className="font-medium tracking-wider transition-color hover:text-yellow-500">
+                        Home
+                    </NavLink>
+
+                </Navbar.Link>
+                <Navbar.Link>
+
+                    <NavLink to='/experience' className={"font-medium tracking-wider transition-color hover:text-yellow-500"}>
+                        Experience
+                    </NavLink>
+
+                </Navbar.Link>
+                <Navbar.Link>
+
+                    <NavLink to='/skills' className={"font-medium tracking-wider transition-color hover:text-yellow-500"}>
+                        Skills
+                    </NavLink>
+
+                </Navbar.Link>
+                <Navbar.Link>
+
+                    <NavLink to='/projects' className={"font-medium tracking-wider transition-color hover:text-yellow-500"}>
+                        Projects
+                    </NavLink>
+
+                </Navbar.Link>
+                <Navbar.Link>
+
+                    <NavLink to='/certifications' className={"font-medium tracking-wider transition-color hover:text-yellow-500"}>
+                        Certifications
+                    </NavLink>
+
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+
+        // <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black max-w-4xl mx-auto">
+        //     <div className='container px-4 mx-auto lg:max-w-4xl flex items-center justify-between'>
+        //         <div>
+        //             <NavLink to="/" className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Md Abu Nasib</NavLink>
+        //         </div>
+        //         <div className="space-x-10">
+        //             <NavLink to="/" className={textStyle}>Home</NavLink>
+        //             <NavLink to="/experience" className={textStyle}>Experience</NavLink>
+        //             <NavLink to="/skills" className={textStyle}>Skills</NavLink>
+        //             <NavLink to="/projects" className={textStyle}>Projects</NavLink>
+        //             <NavLink to="/certifications" className={textStyle}>Certifications</NavLink>
+        //         </div>
+        //         <div>
+        //             <ThemeSwitch />
+        //         </div>
+        //     </div>
+        // </div >
+
+    )
+}
+
+export default Header
