256d13215d92e68d25875a2aac72681e53c35cf5
Sparrow48 <abunasib48@gmail.com>
2023-03-03 11:51:39 +0600

    Add skill section.

--- src/router/Navigation.jsx
+++ src/router/Navigation.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Experience from '../pages/Experience';
+import Skills from '../pages/Skills';
 
 const Navigation = () => {
     return (
@@ -9,6 +10,7 @@ const Navigation = () => {
             <Routes>
                 <Route exact path={'/'} element={<Home />} />
                 <Route exact path={'/experience'} element={<Experience />} />
+                <Route exact path={'/skills'} element={<Skills />} />
             </Routes>
         </div>
     )
--- src/store/index.js
+++ src/store/index.js
@@ -23,6 +23,97 @@ export const experiences = [
         ],
         position: "Software Engineer",
         techStack: ["React.js", "WebSockets", "Redux Toolkit", "AntD", "FCM"],
-        image: "https://media.licdn.com/dms/image/C4D0BAQH7UK0KghPjAA/company-logo_100_100/0/1519888839489?e=1685577600&v=beta&t=fRziZeM01fZ8B9JZzoqo9QWWv5zT_T_72IrPM4jHNVM"
+        // image: "https://media.licdn.com/dms/image/C4D0BAQH7UK0KghPjAA/company-logo_100_100/0/1519888839489?e=1685577600&v=beta&t=fRziZeM01fZ8B9JZzoqo9QWWv5zT_T_72IrPM4jHNVM"
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/DLL.jpeg?token=GHSAT0AAAAAAB5FQU6LRTWE7K7E3AKE22DGZABRKQQ"
     }
+]
+
+export const skills = [
+    {
+        name: "React",
+        level: "expert",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/react.svg?token=GHSAT0AAAAAAB5FQU6KIU5XPBT557TWQZYKZABSABQ",
+        exps: "2 years",
+    },
+    {
+        name: "Redux Toolkit",
+        level: "expert",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/reduxToolkit.png?token=GHSAT0AAAAAAB5FQU6LT64WSWI2XVT7VM76ZABTCPA",
+        exps: "2 years",
+    },
+
+    {
+        name: "Git",
+        level: "expert",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/git-logo.png?token=GHSAT0AAAAAAB5FQU6LS36M4E2SQWMFARRUZABTB2A",
+        exps: "3 years",
+    },
+    {
+        name: "JavaScript",
+        level: "intermediate",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/js.png?token=GHSAT0AAAAAAB5FQU6LHJUPX5ZRCKYKV7BIZABSHXA",
+        exps: "2 years",
+    },
+
+    {
+        name: "Problem Solving",
+        level: "intermediate",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/ps.png?token=GHSAT0AAAAAAB5FQU6K72J4H5VN6HF7SR2AZABSIKA",
+        exps: "100+ solved",
+    },
+
+    {
+        name: "Tailwind",
+        level: "intermediate",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/tailwind.png?token=GHSAT0AAAAAAB5FQU6KGRPCIWINKLXABXCUZABSSAQ",
+        exps: "1 years",
+    },
+    {
+        name: "AntD",
+        level: "intermediate",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/antD.png?token=GHSAT0AAAAAAB5FQU6L7YERMH3UFAXUWHAOZABSSCA",
+        exps: "1 years",
+    },
+    {
+        name: "WebSocket",
+        level: "intermediate",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/webSocket.png?token=GHSAT0AAAAAAB5FQU6LMWKWWRYKVWGC2HV2ZABSSBA",
+        exps: "1 year",
+    },
+    {
+        name: "HTML",
+        level: "expert",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/htmlLogo.png?token=GHSAT0AAAAAAB5FQU6LSQVRX4SQEKHQMY3QZABTCFQ",
+        exps: "2 years",
+    },
+    {
+        name: "CSS",
+        level: "expert",
+        image: "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png",
+        exps: " 2 years",
+    },
+    {
+        name: "Linux",
+        level: "expert",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/linux.png?token=GHSAT0AAAAAAB5FQU6KV4IVR3B66UPW5VCKZABTCNQ",
+        exps: "4 years",
+    },
+    {
+        name: "Node.js",
+        level: "beginner",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/node.png?token=GHSAT0AAAAAAB5FQU6KX4SOJU5FW4ZCS37AZABSGEQ",
+        exps: "beginner",
+    },
+    {
+        name: "Express.js",
+        level: "beginner",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/express.png?token=GHSAT0AAAAAAB5FQU6KWIWH7EJCISRGFJACZABTBQA",
+        exps: "beginner",
+    },
+    {
+        name: "MongoDB",
+        level: "beginner",
+        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/mongodb.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A",
+        exps: "3 months",
+    },
 ]
