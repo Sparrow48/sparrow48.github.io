c3824d0a5ff380396fd4d3a4b99d4e478769ee3b
Sparrow48 <abunasib48@gmail.com>
2024-02-02 07:33:35 +0100

    Update gmail id.

--- src/store/index.js
+++ src/store/index.js
@@ -1,217 +1,274 @@
 export const about = {
-    name: "Md Abu Nasib",
-    jobDescription: "a software developer from Bangladesh. Currently working as a Software Engineer at",
-    company: {
-        name: "eDoctor",
-        link: "https://edoktor.rs/",
-    },
-    description: "I have over 1.5 years of experience in developing software using React.js, Redux, Antd Nest.js and TypeScript.",
-    opening: "Looking for an opportunity where I can use my skills to contribute to an organization’s development team.",
-}
+  name: 'Md Abu Nasib',
+  jobDescription:
+    'a software developer from Bangladesh. Currently working as a Software Engineer at',
+  company: {
+    name: 'eDoctor',
+    link: 'https://edoktor.rs/',
+  },
+  description:
+    'I have almost 2 years of experience in developing software using React.js, Redux, Antd Nest.js and TypeScript.',
+  opening:
+    'Looking for an opportunity where I can use my skills to contribute to an organization’s development team.',
+};
 
 export const experiences = [
-    {
-        companyName: "eDoktor",
-        startDate: "June 2023",
-        endDate: "Present",
-        descriptions: [
-            "Developing scalable Telemedicine applications with cutting-edge technology",
-            "Developed both doctor and admin front-end web applications.",
-            "Resolved issues and developed REST APIs using TypeScript and Nest.js",
-        ],
-        position: "Software Engineer",
-        techStack: ["React.js", "Socket.io", "Redux Toolkit", "AntD", "FCM", 'Nest.js','TypeScript', 'MongoDB'],
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/eDok.png"
-    },
-    {
-        companyName: "Durbin Healthtech",
-        startDate: "May 2022",
-        endDate: "May 2023",
-        descriptions: [
-            "Developing scalable Telemedicine applications with cutting-edge technology",
-            "Optimize application for maximum speed and scalability.",
-            "Developing new features and integrate RESTful APIs."
-        ],
-        position: "Software Engineer",
-        techStack: ["React.js", "Socket.io", "Redux Toolkit", "AntD", "FCM"],
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/DHT.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A"
-    },
-]
+  {
+    companyName: 'eDoktor',
+    startDate: 'June 2023',
+    endDate: 'Present',
+    descriptions: [
+      'Developing scalable Telemedicine applications with cutting-edge technology',
+      'Developed both doctor and admin front-end web applications.',
+      'Resolved issues and developed REST APIs using TypeScript and Nest.js',
+      'Performed data migration',
+    ],
+    position: 'Software Engineer',
+    techStack: [
+      'React.js',
+      'Socket.io',
+      'Redux Toolkit',
+      'AntD',
+      'FCM',
+      'Nest.js',
+      'TypeScript',
+      'MongoDB',
+    ],
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/eDok.png',
+  },
+  {
+    companyName: 'Durbin Healthtech',
+    startDate: 'May 2022',
+    endDate: 'May 2023',
+    descriptions: [
+      'Developing scalable Telemedicine applications with cutting-edge technology',
+      'Optimize application for maximum speed and scalability.',
+      'Developing new features and integrate RESTful APIs.',
+    ],
+    position: 'Software Engineer',
+    techStack: ['React.js', 'Socket.io', 'Redux Toolkit', 'AntD', 'FCM'],
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/DHT.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A',
+  },
+];
 
 export const skills = [
-    {
-        name: "React",
-        level: "expert",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/react.svg?token=GHSAT0AAAAAAB5FQU6KZD3YI45TJUTG5XXQZABUBCQ",
-        exps: "2 years",
-    },
-    {
-        name: "Redux Toolkit",
-        level: "expert",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/reduxToolkit.png?token=GHSAT0AAAAAAB5FQU6LT64WSWI2XVT7VM76ZABTCPA",
-        exps: "2 years",
-    },
+  {
+    name: 'React',
+    level: 'expert',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/react.svg?token=GHSAT0AAAAAAB5FQU6KZD3YI45TJUTG5XXQZABUBCQ',
+    exps: '2 years',
+  },
+  {
+    name: 'Redux Toolkit',
+    level: 'expert',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/reduxToolkit.png?token=GHSAT0AAAAAAB5FQU6LT64WSWI2XVT7VM76ZABTCPA',
+    exps: '2 years',
+  },
 
-    {
-        name: "Git",
-        level: "expert",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/git-logo.png?token=GHSAT0AAAAAAB5FQU6LS36M4E2SQWMFARRUZABTB2A",
-        exps: "3.5 years",
-    },
-    {
-        name: "JavaScript",
-        level: "intermediate",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/js.png?token=GHSAT0AAAAAAB5FQU6KCHJMMVTQCV52LJKQZABUB6Q",
-        exps: "2.5 years",
-    },
+  {
+    name: 'Git',
+    level: 'expert',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/git-logo.png?token=GHSAT0AAAAAAB5FQU6LS36M4E2SQWMFARRUZABTB2A',
+    exps: '3.5 years',
+  },
+  {
+    name: 'JavaScript',
+    level: 'intermediate',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/js.png?token=GHSAT0AAAAAAB5FQU6KCHJMMVTQCV52LJKQZABUB6Q',
+    exps: '2.5 years',
+  },
 
-    {
-        name: "Problem Solving",
-        level: "intermediate",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/ps.png?token=GHSAT0AAAAAAB5FQU6K72J4H5VN6HF7SR2AZABSIKA",
-        exps: "100+ solved",
-    },
+  {
+    name: 'Problem Solving',
+    level: 'intermediate',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/ps.png?token=GHSAT0AAAAAAB5FQU6K72J4H5VN6HF7SR2AZABSIKA',
+    exps: '100+ solved',
+  },
 
-    {
-        name: "Tailwind",
-        level: "intermediate",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/tailwind.png?token=GHSAT0AAAAAAB5FQU6KGRPCIWINKLXABXCUZABSSAQ",
-        exps: "1.5 years",
-    },
-    {
-        name: "AntD",
-        level: "intermediate",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/antD.png?token=GHSAT0AAAAAAB5FQU6L7YERMH3UFAXUWHAOZABSSCA",
-        exps: "1.5 years",
-    },
-    {
-        name: "WebSocket",
-        level: "intermediate",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/webSocket.png?token=GHSAT0AAAAAAB5FQU6LMWKWWRYKVWGC2HV2ZABSSBA",
-        exps: "1.5 year",
-    },
-    {
-        name: "HTML",
-        level: "expert",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/htmlLogo.png?token=GHSAT0AAAAAAB5FQU6LSQVRX4SQEKHQMY3QZABTCFQ",
-        exps: "2.5 years",
-    },
-    {
-        name: "CSS",
-        level: "expert",
-        image: "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png",
-        exps: " 2.5 years",
-    },
-    {
-        name: "Linux",
-        level: "expert",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/linux.png?token=GHSAT0AAAAAAB5FQU6KV4IVR3B66UPW5VCKZABTCNQ",
-        exps: "4.5 years",
-    },
-    {
-        name: "Node.js",
-        level: "beginner",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/node.png?token=GHSAT0AAAAAAB5FQU6KVR6BJYJLXOCXAIG2ZABUC2Q",
-        exps: "1 year",
-    },
-    {
-        name: "Express.js",
-        level: "beginner",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/express.png?token=GHSAT0AAAAAAB5FQU6KWIWH7EJCISRGFJACZABTBQA",
-        exps: "1 year",
-    },
-    {
-        name: "MongoDB",
-        level: "beginner",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/mongodb.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A",
-        exps: "1 year",
-    },
-    {
-        name: "Nest.js",
-        level: "beginner",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/nest.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A",
-        exps: "6 months",
-    },
-    {
-        name: "TypeScript",
-        level: "beginner",
-        image: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/TS.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A",
-        exps: "6 months",
-    },
-]
+  {
+    name: 'Tailwind',
+    level: 'intermediate',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/tailwind.png?token=GHSAT0AAAAAAB5FQU6KGRPCIWINKLXABXCUZABSSAQ',
+    exps: '1.5 years',
+  },
+  {
+    name: 'AntD',
+    level: 'intermediate',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/antD.png?token=GHSAT0AAAAAAB5FQU6L7YERMH3UFAXUWHAOZABSSCA',
+    exps: '1.5 years',
+  },
+  {
+    name: 'WebSocket',
+    level: 'intermediate',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/webSocket.png?token=GHSAT0AAAAAAB5FQU6LMWKWWRYKVWGC2HV2ZABSSBA',
+    exps: '1.5 year',
+  },
+  {
+    name: 'HTML',
+    level: 'expert',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/htmlLogo.png?token=GHSAT0AAAAAAB5FQU6LSQVRX4SQEKHQMY3QZABTCFQ',
+    exps: '2.5 years',
+  },
+  {
+    name: 'CSS',
+    level: 'expert',
+    image:
+      'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png',
+    exps: ' 2.5 years',
+  },
+  {
+    name: 'Linux',
+    level: 'expert',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/linux.png?token=GHSAT0AAAAAAB5FQU6KV4IVR3B66UPW5VCKZABTCNQ',
+    exps: '4.5 years',
+  },
+  {
+    name: 'Node.js',
+    level: 'beginner',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/node.png?token=GHSAT0AAAAAAB5FQU6KVR6BJYJLXOCXAIG2ZABUC2Q',
+    exps: '1 year',
+  },
+  {
+    name: 'Express.js',
+    level: 'beginner',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/express.png?token=GHSAT0AAAAAAB5FQU6KWIWH7EJCISRGFJACZABTBQA',
+    exps: '1 year',
+  },
+  {
+    name: 'MongoDB',
+    level: 'beginner',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/mongodb.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A',
+    exps: '1 year',
+  },
+  {
+    name: 'Nest.js',
+    level: 'beginner',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/nest.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A',
+    exps: '6 months',
+  },
+  {
+    name: 'TypeScript',
+    level: 'beginner',
+    image:
+      'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/TS.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A',
+    exps: '6 months',
+  },
+];
 
 export const projects = {
-    "eDoktor": {
-        title: 'eDoktor',
-        description: 'eDoktor is a telemedicine platform that connects patients with doctors.',
-        longDescription: 'eDoktor is a telemedicine platform that connects patients with doctors. Patients can book appointments with doctors and get a consultation from anywhere. Doctors can manage their appointments, patients, and prescriptions.',
-        workDescriptions: [
-            "Developing scalable Telemedicine applications with cutting-edge technology",
-            "Optimize application for maximum speed and scalability.",
-            "Developed both doctor and admin front-end web applications.",
-            "Integrate RESTful APIs.",
-            "Resolved issues and developed REST APIs using TypeScript and Nest.js",
-            'Integrate a video calling feature using Zoom Video SDK.',
-            'Integrate a notification system using WebSocket and FCM.',
-            'Developed a system for doctors to manage their appointments, patients, prescriptions.',
-        ],
-        techStack: ['React.js', 'Redux Toolkit', 'AntD', 'WebSocket', 'FCM', 'Nest.js','TypeScript', 'MongoDB'],
-        link: 'https://edoktor.rs/',
-        associatedWith: {
-            companyName: "eDoktor.",
-            companyLogo: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/eDok.png"
-        }
+  eDoktor: {
+    title: 'eDoktor',
+    description:
+      'eDoktor is a telemedicine platform that connects patients with doctors.',
+    longDescription:
+      'eDoktor is a telemedicine platform that connects patients with doctors. Patients can book appointments with doctors and get a consultation from anywhere. Doctors can manage their appointments, patients, and prescriptions.',
+    workDescriptions: [
+      'Developing scalable Telemedicine applications with cutting-edge technology',
+      'Optimize application for maximum speed and scalability.',
+      'Developed both doctor and admin front-end web applications.',
+      'Integrate RESTful APIs.',
+      'Resolved issues and developed REST APIs using TypeScript and Nest.js',
+      'Integrate a video calling feature using Zoom Video SDK.',
+      'Integrate a notification system using WebSocket and FCM.',
+      'Developed a system for doctors to manage their appointments, patients, prescriptions.',
+      'Performed data migration.',
+    ],
+    techStack: [
+      'React.js',
+      'Redux Toolkit',
+      'AntD',
+      'WebSocket',
+      'FCM',
+      'Nest.js',
+      'TypeScript',
+      'MongoDB',
+    ],
+    link: 'https://edoktor.rs/',
+    associatedWith: {
+      companyName: 'eDoktor.',
+      companyLogo:
+        'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/eDok.png',
     },
-    "retinaLms": {
-        title: 'RetinaLMS',
-        description: 'An admission coaching-centric scalable learning management system.',
-        longDescription: 'RetinaLMS is a scalable learning management system, with admission, online payment, book distribution, and an online exam system.',
-        workDescriptions: [
-            "Optimize application for maximum speed and scalability.",
-            "Develop new features and functionality on the website.",
-            "Integrate RESTful APIs.",
-            "Fixed existing bugs."
-        ],
-        techStack: ['React.js', 'Redux Toolkit', 'AntD'],
-        link: 'https://retinalms.com',
-        associatedWith: {
-            companyName: "Durbin Labs Ltd.",
-            companyLogo: "https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/DHT.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A"
-        }
+  },
+  retinaLms: {
+    title: 'RetinaLMS',
+    description:
+      'An admission coaching-centric scalable learning management system.',
+    longDescription:
+      'RetinaLMS is a scalable learning management system, with admission, online payment, book distribution, and an online exam system.',
+    workDescriptions: [
+      'Optimize application for maximum speed and scalability.',
+      'Develop new features and functionality on the website.',
+      'Integrate RESTful APIs.',
+      'Fixed existing bugs.',
+    ],
+    techStack: ['React.js', 'Redux Toolkit', 'AntD'],
+    link: 'https://retinalms.com',
+    associatedWith: {
+      companyName: 'Durbin Labs Ltd.',
+      companyLogo:
+        'https://raw.githubusercontent.com/Sparrow48/nasib/main/src/assets/DHT.png?token=GHSAT0AAAAAAB5FQU6K4VTIIVASLWH2WKG6ZABSR4A',
     },
-    "e-shop": {
-        title: 'e-shop',
-        description: 'An e-commerce site.',
-        longDescription: 'A typical e-commerce website where users can create accounts and make purchase requests for products. Users can view their purchase history. Anyone can create a list using the add to card option and after logging into the system the user can request a purchase of the cart list through the checkout option.',
-        workDescriptions: [
-            "Developed both front-end and back-end by me.",
-            "Developed RESTful APIs using JavaScript’s expressJs framework",
-            "Implemented data storage solutions using MongoDB.",
-            "Integrate RESTful APIs.",
-        ],
-        techStack: ['Node.js', 'Express.js', 'React.js', 'MongoDB', 'Redux Toolkit', 'Tailwind CSS'],
-        link: 'https://e-shop-one-rho.vercel.app/',
-        associatedWith: {
-            companyName: "",
-            companyLogo: ""
-        }
+  },
+  'e-shop': {
+    title: 'e-shop',
+    description: 'An e-commerce site.',
+    longDescription:
+      'A typical e-commerce website where users can create accounts and make purchase requests for products. Users can view their purchase history. Anyone can create a list using the add to card option and after logging into the system the user can request a purchase of the cart list through the checkout option.',
+    workDescriptions: [
+      'Developed both front-end and back-end by me.',
+      'Developed RESTful APIs using JavaScript’s expressJs framework',
+      'Implemented data storage solutions using MongoDB.',
+      'Integrate RESTful APIs.',
+    ],
+    techStack: [
+      'Node.js',
+      'Express.js',
+      'React.js',
+      'MongoDB',
+      'Redux Toolkit',
+      'Tailwind CSS',
+    ],
+    link: 'https://e-shop-one-rho.vercel.app/',
+    associatedWith: {
+      companyName: '',
+      companyLogo: '',
     },
-}
+  },
+};
 
 const issuer = {
-    udemy: {
-        NAME: "Udemy",
-        IMAGE: "https://media.licdn.com/dms/image/C4D0BAQFQr9e68bBOPQ/company-logo_100_100/0/1626275253364?e=1685577600&v=beta&t=JXMAOg5CubQBVlGK0m4M4acDFEtkGpE6qkLDsFvHmNg",
-    },
-}
+  udemy: {
+    NAME: 'Udemy',
+    IMAGE:
+      'https://media.licdn.com/dms/image/C4D0BAQFQr9e68bBOPQ/company-logo_100_100/0/1626275253364?e=1685577600&v=beta&t=JXMAOg5CubQBVlGK0m4M4acDFEtkGpE6qkLDsFvHmNg',
+  },
+};
 
 export const certificates = [
-    {
-        title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
-        issuer: issuer.udemy.NAME,
-        issuerImage: issuer.udemy.IMAGE,
-        issueDate: "February 08, 2022",
-        certLink: "https://www.udemy.com/certificate/UC-17d3a0eb-dc75-4c79-a46a-dc34f374b7e7/",
-        skills: ["react", "redux", "rtk-query", "javascript", "frontend"],
-    },
-]
+  {
+    title: 'React - The Complete Guide (incl Hooks, React Router, Redux)',
+    issuer: issuer.udemy.NAME,
+    issuerImage: issuer.udemy.IMAGE,
+    issueDate: 'February 08, 2022',
+    certLink:
+      'https://www.udemy.com/certificate/UC-17d3a0eb-dc75-4c79-a46a-dc34f374b7e7/',
+    skills: ['react', 'redux', 'rtk-query', 'javascript', 'frontend'],
+  },
+];
